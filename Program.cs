b2857e5d5d614134db98edcc20c960b47b73eaeb
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 07:42:27 +0530

    Added:Employee App file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpolyeePresentandAbsent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WElcome to Employee App");
+        }
+    }
+}
