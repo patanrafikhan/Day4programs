3a5410c42d3d38eb9f0642456f1a7b52e5739ada
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 07:56:25 +0530

    Added :UC1 Employee is Present and Absent

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ namespace EmpolyeePresentandAbsent
         static void Main(string[] args)
         {
             Console.WriteLine("WElcome to Employee App");
+            Console.WriteLine("Enter Employee name :");
+            string name=Console.ReadLine();
+
+            Random ran= new Random();
+            int n= ran.Next();
+
+            if(n == 0 )
+            {
+                Console.WriteLine("Empolee is Absent ");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
         }
     }
 }
