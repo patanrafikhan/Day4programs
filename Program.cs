68562ceffa5486b40233be95b7c2b305d3cb5a25
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 08:21:48 +0530

    Added :UC2 Employee Wage Computation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace EmpolyeePresentandAbsent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WElcome to Employee App");
+            Console.WriteLine("WElcome to Employee Wage Computation");
+           
+            Random random= new Random();
+            int n=random.Next(0, 2);
+            int WagePerHour = 20;
+            int FullDayHour = 8;
+
+            if (n == 0)
+            {
+                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee Wage is : "+WagePerHour*FullDayHour);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
