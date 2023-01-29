954883ce25c634939b76175fe26107190c364205
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 09:43:16 +0530

    Added UC4 Employee wage Switch Case

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace EmpolyeePresentandAbsent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WElcome to Employee App");
+            Console.WriteLine("WElcome to Employee Wage Computation");
+            Random ran=new Random();
+            int n=ran.Next(0, 3);
+            int PerHour_Wage = 20;
+            int FullDay_Hour = 8;
+            int HalfDay_Hour= 4;
+            
+            
+                switch (n)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is Present and Worked HalfDay :"+ HalfDay_Hour * PerHour_Wage);
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is Present and Worked FullDay :"+ FullDay_Hour * PerHour_Wage);
+                        break;
+                }
         }
     }
 }
