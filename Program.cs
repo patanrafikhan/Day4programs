beb34c04eb85a97c52b0f0efcec2aa78eccfde35
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 08:45:58 +0530

    Added :UC3 Added Employee PartTime Wage

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace EmpolyeePresentandAbsent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WElcome to Employee App");
+            Console.WriteLine("WElcome to Part Time Employee & Wage ");
+
+            Random ran = new Random();
+            int n =ran.Next(0,3);
+            int WagePerHour = 20;
+            int FullDayHour = 8;
+            int PartTimeHour = 4;
+
+            if (n == 0)
+            {
+                Console.WriteLine("Employee is Present and Worked FullTime");
+                Console.WriteLine("Daily Wage is : " + WagePerHour * FullDayHour);
+            }
+            else if (n == 1) 
+            {
+                Console.WriteLine("Employee is Present and Worked PartTime");
+                Console.WriteLine("PartTime Wage is :"+WagePerHour * PartTimeHour);
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+
         }
     }
 }
